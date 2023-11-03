e546b143aaee31b1abab65189d621728b3b5b9a5
khairul islam <49713740+Khairul-islam98@users.noreply.github.com>
2023-11-03 03:24:27 +0000

    added objects

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -20,8 +20,58 @@ const regularUser = {
 // console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 ={1: 'a', 2: 'b'}
-const obj2 ={3: 'c', 4: 'c'}
+const obj2 ={3: 'a', 4: 'b'}
+const obj3 ={4: 'a', 5: 'b'}
 
 // const obj3 = Object.assign(obj1, obj2)
-const obj3 = Object.assign({}, obj1, obj2) // batter way to combine 
-console.log(obj3);
+// const obj4 = Object.assign({}, obj1, obj2, obj3) // batter way to combine 
+const obj4 ={...obj1, ...obj2, ...obj3}
+// console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "k@gmail.com"
+    },
+    {
+        id: 1,
+        email: "k@gmail.com"
+    },
+    {
+        id: 1,
+        email: "k@gmail.com"
+    },
+]
+users[1].email
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    courseName: 'js development',
+    price: '999',
+    courseInstructor: 'khairul islam'
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = 'khairul')
+
+// {
+//     'name': 'khairul islam',
+//     'courseName': 'js in bangla',
+//     'price': 'free'
+// }
